86a6263a95834442992a2aec774434f06ffb7e5f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-18 21:17:22 +0300

    init Breathe

--- HowToCreateBreatheAnimation/ContentView.swift
+++ HowToCreateBreatheAnimation/ContentView.swift
@@ -9,8 +9,175 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isCenter = true
+    @State var isCenter2 = true
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ZStack {
+                Spacer()
+                
+                // Orange group
+                Group {
+                    Circle()
+                        .frame(width: 151, height: 501)
+                        .foregroundColor(Color.blue.opacity(0.5))
+                        .offset(x: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+                    
+                    Circle()
+                        .frame(width: 151, height: 501)
+                        .foregroundColor(Color.blue.opacity(0.5))
+                        .offset(x: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+
+                    Circle()
+                        .frame(width: 151, height: 501)
+                        .foregroundColor(Color.blue.opacity(0.5))
+                        .offset(y: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+
+                    Circle()
+                        .frame(width: 151, height: 501)
+                        .foregroundColor(Color.blue.opacity(0.5))
+                        .offset(y: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+                }
+                
+                // Purple group
+                Group {
+                    Circle()
+                        .frame(width: 450, height: 150)
+                        .foregroundColor(Color.purple.opacity(0.5))
+                        .offset(x: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                    
+                    Circle()
+                        .frame(width: 450, height: 150)
+                        .foregroundColor(Color.purple.opacity(0.5))
+                        .offset(x: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 450, height: 150)
+                        .foregroundColor(Color.purple.opacity(0.5))
+                        .offset(y: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 450, height: 150)
+                        .foregroundColor(Color.purple.opacity(0.5))
+                        .offset(y: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                }
+                
+                // Orange group
+                Group {
+                    Circle()
+                        .frame(width: 350, height: 150)
+                        .foregroundColor(Color.orange.opacity(0.5))
+                        .offset(x: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+                    
+                    Circle()
+                        .frame(width: 350, height: 150)
+                        .foregroundColor(Color.orange.opacity(0.5))
+                        .offset(x: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+
+                    Circle()
+                        .frame(width: 350, height: 150)
+                        .foregroundColor(Color.orange.opacity(0.5))
+                        .offset(y: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+
+                    Circle()
+                        .frame(width: 350, height: 150)
+                        .foregroundColor(Color.orange.opacity(0.5))
+                        .offset(y: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 360 : 0))
+                }
+                
+                // Pink group
+                Group {
+                    Circle()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(Color.pink.opacity(0.5))
+                        .offset(x: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                    
+                    Circle()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(Color.pink.opacity(0.5))
+                        .offset(x: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(Color.pink.opacity(0.5))
+                        .offset(y: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(Color.pink.opacity(0.5))
+                        .offset(y: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                }
+                
+                // Red group
+                Group {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.red.opacity(0.5))
+                        .offset(x: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                    
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.red.opacity(0.5))
+                        .offset(x: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.red.opacity(0.5))
+                        .offset(y: isCenter ? 0 : 50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color.red.opacity(0.5))
+                        .offset(y: isCenter ? 0 : -50)
+                        .rotationEffect(Angle(degrees: isCenter2 ? 0 : 360))
+                }
+                
+                
+                Circle()
+                    .frame(width: isCenter ? 149 : 199, height: isCenter ? 149 : 199)
+                    .foregroundColor(.white)
+                
+                Text("Breathe")
+                    .font(.custom("Georgia", size: 42))  
+                    .foregroundColor(.gray)
+                
+                
+                
+            }
+            
+            .onAppear() {
+                let animation = Animation.linear(duration: 3)
+                withAnimation(animation.repeatForever(autoreverses: true)) {
+                    self.isCenter = false
+                }
+                withAnimation(animation.repeatForever(autoreverses: true)) {
+                    self.isCenter2 = false
+                }
+            }
+            
+            
+            
+            Spacer()
+        }
     }
 }
 
